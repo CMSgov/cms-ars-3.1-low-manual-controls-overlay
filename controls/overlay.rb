bb9a464d70d076e8fde543a7d2006d7cd1594dbe
ejaronne <34140975+ejaronne@users.noreply.github.com>
2021-03-15 13:29:17 -0400

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,1 +1,13 @@
+include_controls "cms-ars-3.1-manual-controls-baseline" do
+  
+  control "CMS-ARS-3.1-CM-07(04)" do
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end 
+  
+  control "CMS-ARS-3.1-CM-07(05)ML" do
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end 
 
+end
