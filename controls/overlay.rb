d88c34cccc547b9901e69a308d4a3f4ef4790c05
ejaronne <34140975+ejaronne@users.noreply.github.com>
2021-03-16 09:39:38 -0400

    add overlay content for moderate ARS

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,13 +1,413 @@
 include_controls "cms-ars-3.1-manual-controls-baseline" do
-  
-  control "CMS-ARS-3.1-CM-07(04)" do
+
+  control 'CMS-ARS-3.1-AC-02(05)' do 
     impact 0.0
   	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
-  end 
-  
-  control "CMS-ARS-3.1-CM-07(05)ML" do
+  end
+
+  control 'CMS-ARS-3.1-AC-02(11)' do 
     impact 0.0
   	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
-  end 
+  end
+
+  control 'CMS-ARS-3.1-AC-02(12)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AC-02(13)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AC-06(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AC-10' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AC-18(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AC-18(05)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-03(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-05(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-05(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-06(05)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-06(06)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-09(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-09(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-10' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-12(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-AU-12(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CA-02(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CA-02(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-02(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-03(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-04(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-05(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-05(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-05(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-06(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-06(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-07(05)H' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-08(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CM-08(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-02(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-02(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-02(05)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-03(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-04(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-06(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-07(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-08(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-08(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-09(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-09(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-09(05)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-CP-10(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IA-02(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IA-02(09)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IR-02(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IR-02(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IR-04(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-IR-05(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MA-02(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MA-03(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MA-04(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MA-05(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MP-06(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MP-06(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-MP-06(03)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-03(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-06(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-08(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-11(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-13(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-13(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-15(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PE-18' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-PS-04(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-RA-05(04)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SA-12' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SA-16' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SA-17' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-03' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-07(08)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-07(18)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-07(21)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-12(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SC-24' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-02(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-05(01)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-06' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-07(02)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-07(05)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'CMS-ARS-3.1-SI-07(14)' do 
+    impact 0.0
+  	desc 'caveat', 'This is Not Applicable since the related security control is not applied to this system categorization in CMS ARS 3.1'
+  end
 
 end
